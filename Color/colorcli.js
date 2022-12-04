6b903fd7d8b5c3cc91fd74ca1e1c1e163b610ce0
Mitchell Willemsen <45740908+oSketchMastah@users.noreply.github.com>
2022-12-04 06:18:30 -0500

    Add files via upload

---
+++ Color/colorcli.js
@@ -0,0 +1,22 @@
+
+const ElementSpecs = ['m', 'box', 'nav', 'active', 'target'];
+
+function GetBackgroundVar(spec) { return '--color-' + spec + 'bg'; }
+function GetTextVar(spec) { return '--color-' + spec + 'txt'; }
+function GetVar(id) {
+if ((id & 1) == 0) {
+	return GetBackgroundVar(ElementSpecs[id >> 1]);
+} else {
+	return GetTextVar(ElementSpecs[id >> 1]);
+}
+}
+
+var hexc = localStorage.colors;
+if (hexc) {
+	let root = document.querySelector(':root').style;
+	for (let i = 0; i < 10; i++) {
+		let cssvar= GetVar(i);
+		root.setProperty(cssvar, '#' + hexc.substring(0, 6));
+		hexc = hexc.substring(6);
+	}
+}
